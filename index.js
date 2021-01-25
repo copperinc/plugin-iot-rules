e860824254a8d79eab5893d2193525d71435b50b
filmaj <maj.fil@gmail.com>
2021-01-25 15:26:19 -0600

    use absolute paths everywhere, dont pull in inventory twice when it gets passed in for free

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let inventory = require('@architect/inventory');
 let { createLambda } = require('@architect/package/src/visitors/utils');
 let read = require('@architect/inventory/src/read');
 let defaultFunctionConfig = require('@architect/inventory/src/defaults/function-config');
@@ -7,10 +6,10 @@ let invokeLambda = require('@architect/sandbox/src/invoke-lambda');
 const { prompt } = require('enquirer');
 const { join } = require('path');
 
-module.exports = async function macroIotRules (arc, cfn /* , stage='staging' */) {
+module.exports = async function macroIotRules (arc, cfn, stage = 'staging', inventory) {
     // modify main role to allow lambdas to publish to iot topics
     if (arc.rules) {
-        let appInv = await inventory({});
+        const cwd = inventory.inv._project.src;
         cfn.Resources.Role.Properties.Policies.push({
             PolicyName: 'ArcIoTDataPolicy',
             PolicyDocument: {
@@ -25,12 +24,12 @@ module.exports = async function macroIotRules (arc, cfn /* , stage='staging' */)
             }
         });
         arc.rules.forEach(rule => {
-            let code = `./src/rules/${rule[0]}`;
+            let code = join(cwd, 'src', 'rules', rule[0]);
             let name = toLogicalID(rule.shift());
             let query = rule.join(' ').trim();
             let functionConfig = getFunctionConfig(code);
             let functionDefn = createLambda({
-                inventory: appInv,
+                inventory,
                 lambda: {
                     src: code,
                     config: functionConfig
@@ -50,10 +49,10 @@ module.exports = async function macroIotRules (arc, cfn /* , stage='staging' */)
 };
 
 module.exports.create = function IoTRulesCreate (inventory) {
-    const cwd = inventory.inv._project.src
+    const cwd = inventory.inv._project.src;
     return inventory.inv._project.arc.rules.map((rule) => {
-        let rulesSrc = join(cwd, 'src', 'rules', rule[0])
-        let functionConfig = getFunctionConfig(rulesSrc)
+        let rulesSrc = join(cwd, 'src', 'rules', rule[0]);
+        let functionConfig = getFunctionConfig(rulesSrc);
         return {
             src: rulesSrc,
             config: functionConfig,
